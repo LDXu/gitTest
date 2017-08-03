4d731a9c068c04d6d85388210858370ede2d6165
XuBill <123495351@qq.com>
2017-08-03 15:34:17 +0800

    test4

--- GHPay-demo/GHPay-demo/ViewController.m
+++ GHPay-demo/GHPay-demo/ViewController.m
@@ -31,6 +31,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    {
+        NSMutableArray *ar = [NSMutableArray new];
+    }
     // Do any additional setup after loading the view, typically from a nib.
     
     [self.view addSubview:self.tableView];
